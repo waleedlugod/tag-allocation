7a640497a5f8bb435952ee7821fad98e47f6a957
Waleed Lugod <lugodwaleed@gmail.com>
2025-05-05 11:11:07 +0800

    population database

--- data.py
+++ data.py
@@ -2,14 +2,40 @@ import random
 import string
 import pandas as pd
 
-N=5
-max_cost = 50
+LOCATION_CNT = 10
 
-b_locs = []
-b_costs = []
-for i in range(10):
-    b_locs.append(''.join(random.choices(string.ascii_uppercase + string.digits, k=N)))
-    b_costs.append(random.randint(1, max_cost))
+DATABASE_CNT = 10
+LOCATION_LEN = 5
+MAX_COST = 50
 
-billboards ={'locations': b_locs, 'cost': b_costs}
-pd.DataFrame(billboards).to_csv("billboards.csv")
+POPULATION_CNT = 10
+MAX_TIMESTAMP = 100
+
+
+locations = []
+for i in range(LOCATION_CNT):
+    locations.append(
+        "".join(random.choices(string.ascii_uppercase + string.digits, k=LOCATION_LEN))
+    )
+
+
+# billboard database
+billboards = []
+for i in range(DATABASE_CNT):
+    location = random.choice(locations)
+    cost = random.randint(1, MAX_COST)
+    billboards.append([location, cost])
+pd.DataFrame(billboards, columns=["location", "cost"]).rename_axis(index="id").to_csv(
+    "billboards.csv"
+)
+
+# population database
+population = []
+for i in range(POPULATION_CNT):
+    location = random.choice(locations)
+    timestamp_start = random.randint(0, MAX_TIMESTAMP - 1)
+    timestamp_stop = random.randint(timestamp_start, MAX_TIMESTAMP)
+    population.append([location, timestamp_start, timestamp_stop])
+pd.DataFrame(
+    population, columns=["location", "timestamp_start", "timestamp_stop"]
+).rename_axis(index="id").to_csv("population.csv")

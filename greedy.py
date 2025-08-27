b2771fb83c778359bd3b796c0f7d3db55cae7540
Waleed Lugod <lugodwaleed@gmail.com>
2025-08-27 14:53:00 +0800

    comment out required tag allocation greedy

--- greedy.py
+++ greedy.py
@@ -26,27 +26,28 @@ total_influence = 0
 billboards = billboards_df.to_numpy()
 slots = slots_df.to_numpy()
 influence_table = influence_table_df.to_numpy()
-i = 0
-while (
-    allocated_slots_cnt < len(Q) and i < len(influence_table) and total_cost < MAX_COST
-):
-    influence = influence_table[i]
-    slot = int(influence[3])
-    billboard = int(slots[slot][1])
-    cost = int(billboards[billboard][2])
-    tag = int(influence[2])
-    if (
-        Q[slot] == -1  # slot is free
-        and influence[1] > 0  # influence of slot is greater than 0
-        and total_cost + cost <= MAX_COST
-        and tag not in allocated_tags
-    ):
-        Q[slot] = tag
-        allocated_slots_cnt += 1
-        allocated_tags.add(tag)
-        total_cost += cost
-        total_influence += influence[1]
-    i += 1
+# if each tags needs to be allocated to a slot first
+# i = 0
+# while (
+#     allocated_slots_cnt < len(Q) and i < len(influence_table) and total_cost < MAX_COST
+# ):
+#     influence = influence_table[i]
+#     slot = int(influence[3])
+#     billboard = int(slots[slot][1])
+#     cost = int(billboards[billboard][2])
+#     tag = int(influence[2])
+#     if (
+#         Q[slot] == -1  # slot is free
+#         and influence[1] > 0  # influence of slot is greater than 0
+#         and total_cost + cost <= MAX_COST
+#         and tag not in allocated_tags
+#     ):
+#         Q[slot] = tag
+#         allocated_slots_cnt += 1
+#         allocated_tags.add(tag)
+#         total_cost += cost
+#         total_influence += influence[1]
+#     i += 1
 
 i = 0
 while (

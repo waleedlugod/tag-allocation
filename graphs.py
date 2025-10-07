800458fe9194c0fdb322ec479579a50f3d816573
Waleed Lugod <lugodwaleed@gmail.com>
2025-10-07 11:48:28 +0800

    multiple figures

--- graphs.py
+++ graphs.py
@@ -9,7 +9,7 @@ performances = []
 avg_approx_ratios = []
 avg_costs = []
 
-brute_avg_cost = output.readline().split(" ")[-1]
+brute_avg_cost = float(output.readline().split(" ")[-1])
 for _ in range(HEURISTICS_CNT - 1):
     performances.append(float(output.readline().split(" ")[-1]))
     avg_approx_ratios.append(float(output.readline().split(" ")[-1]))
@@ -17,13 +17,25 @@ for _ in range(HEURISTICS_CNT - 1):
 
 
 x = np.arange(HEURISTICS_CNT - 1)
-width = 0.25
+width = 0.35
 print(performances)
 
+plt.figure()
 plt.bar(x - width, performances, width, color="green")
 plt.bar(x, avg_approx_ratios, width, color="orange")
 plt.xticks(x, ["Greedy (Influence)", "Greedy (Cost)", "Greedy (Influence/Cost)"])
 plt.xlabel("Heuristics")
 plt.ylabel("Percent")
-plt.legend(["Correctness", "Avg Approximation Ratio"])
+plt.legend(["Performance", "Avg Approximation Ratio"])
+plt.title("Correctness and Approximation Ratios of Greedys")
+
+plt.figure()
+plt.bar(x, avg_costs, width, color="orange")
+plt.xticks(x, ["Greedy (Influence)", "Greedy (Cost)", "Greedy (Influence/Cost)"])
+plt.xlabel("Heuristics")
+plt.ylabel("Avg Cost")
+plt.title("Average Costs of Greedys")
+axes = plt.gca()
+axes.set_ylim([0, brute_avg_cost])
+
 plt.show()

5c27a7600c6c235f3182e1e2533cb3376e81d832
Waleed Lugod <lugodwaleed@gmail.com>
2025-11-26 15:48:50 +0800

    ignore broken data

--- graphs.py
+++ graphs.py
@@ -42,36 +42,43 @@ axes.set_ylim([0, brute_avg_cost])
 # influence distribution
 # note: graph is right skewed as not all users contribute to the final influence value (i.e. 0 value)
 influences = open("influences.txt", "r").read().splitlines()
-influences = [float(_.replace("\x00", "")) for _ in influences]
+influences = [float(_) for _ in influences if "\x00" not in _ and _ != "" and "." in _]
 plt.figure()
-plt.hist(influences)
+plt.hist(influences, rwidth=0.9)
 plt.xlabel("Influence")
 plt.ylabel("Count")
 plt.title("Distribution of Influence Values From Influence Table")
 
 raw_influences = open("raw_influences.txt", "r").read().splitlines()
-raw_influences = [float(_.replace("\x00", "")) for _ in raw_influences]
+raw_influences = [
+    float(_) for _ in raw_influences if "\x00" not in _ and _ != "" and "." in _
+]
 plt.figure()
-plt.hist(raw_influences)
+plt.hist(raw_influences, rwidth=0.9)
 plt.xlabel("Influence")
 plt.ylabel("Count")
 plt.title("Distribution of Population Influence Values")
 
-
 # influence distribution (all test cases)
 # note: graph is right skewed as not all users contribute to the final influence value (i.e. 0 value)
-influences = open("influences_all.txt", "r").read().splitlines()
-influences = [float(_.replace("\x00", "")) for _ in influences]
+influences_all = open("influences_all.txt", "r").read().splitlines()
+influences_all = [
+    float(_) for _ in influences_all if "\x00" not in _ and _ != "" and "." in _
+]
 plt.figure()
-plt.hist(influences)
+plt.hist(influences_all, rwidth=0.9)
 plt.xlabel("Influence")
 plt.ylabel("Count")
 plt.title("Distribution of Influence Values From Influence Table (All Tests)")
 
-raw_influences = open("raw_influences_all.txt", "r").read().splitlines()
-raw_influences = [float(_.replace("\x00", "")) for _ in raw_influences]
+raw_influences_all = open("raw_influences_all.txt", "r").read().splitlines()
+raw_influences_all = [
+    float(_) for _ in raw_influences_all if "\x00" not in _ and _ != "" and "." in _
+]
+mean = np.mean(raw_influences_all)
+std = np.std(raw_influences_all)
 plt.figure()
-plt.hist(raw_influences)
+plt.hist(raw_influences_all, rwidth=0.9)
 plt.xlabel("Influence")
 plt.ylabel("Count")
 plt.title("Distribution of Population Influence Values (All Tests)")

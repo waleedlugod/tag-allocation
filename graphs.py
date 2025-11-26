c7b828c93a8f9174b76e4e0b2da92b664ccbc875
Waleed Lugod <lugodwaleed@gmail.com>
2025-11-26 14:44:56 +0800

    ignore nulls

--- graphs.py
+++ graphs.py
@@ -42,7 +42,7 @@ axes.set_ylim([0, brute_avg_cost])
 # influence distribution
 # note: graph is right skewed as not all users contribute to the final influence value (i.e. 0 value)
 influences = open("influences.txt", "r").read().splitlines()
-influences = [float(_) for _ in influences]
+influences = [float(_.replace("\x00", "")) for _ in influences]
 plt.figure()
 plt.hist(influences)
 plt.xlabel("Influence")
@@ -50,11 +50,30 @@ plt.ylabel("Count")
 plt.title("Distribution of Influence Values From Influence Table")
 
 raw_influences = open("raw_influences.txt", "r").read().splitlines()
-raw_influences = [float(_) for _ in raw_influences]
+raw_influences = [float(_.replace("\x00", "")) for _ in raw_influences]
 plt.figure()
 plt.hist(raw_influences)
 plt.xlabel("Influence")
 plt.ylabel("Count")
 plt.title("Distribution of Population Influence Values")
+
+
+# influence distribution (all test cases)
+# note: graph is right skewed as not all users contribute to the final influence value (i.e. 0 value)
+influences = open("influences_all.txt", "r").read().splitlines()
+influences = [float(_.replace("\x00", "")) for _ in influences]
+plt.figure()
+plt.hist(influences)
+plt.xlabel("Influence")
+plt.ylabel("Count")
+plt.title("Distribution of Influence Values From Influence Table (All Tests)")
+
+raw_influences = open("raw_influences_all.txt", "r").read().splitlines()
+raw_influences = [float(_.replace("\x00", "")) for _ in raw_influences]
+plt.figure()
+plt.hist(raw_influences)
+plt.xlabel("Influence")
+plt.ylabel("Count")
+plt.title("Distribution of Population Influence Values (All Tests)")
 
 plt.show()
